825f52479c15a7ef166c7ebbb220e954e001b966
Abanoub Zaky <zaky.abanoub@gmail.com>
2023-01-15 03:44:36 +0200

    add header responsive - to complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "../assets/logo.svg";
 const Header = () => {
   return (
     <header className="p-4">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="img-container">
           <img src={Logo} alt="little lemon logo" />
         </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import HamburgerMenu from "../assets/🦆 icon _hamburger menu.svg";
+import CloseIcon from "../assets/x-png.jpg";
 
 const LINKS = [
   {
@@ -35,9 +37,28 @@ const LINKS = [
 ];
 
 const Nav = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const showLinksHandler = () => {
+    setShowLinks((prevState) => !prevState);
+  };
+
+  // const hideLinksHandler = () => {
+  //   document.body.classList.remove("overflow-hidden");
+  //   setShowLinks(false);
+  // };
+
+  useEffect(() => {
+    if (showLinks === true) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [showLinks]);
+
   return (
     <nav>
-      <ul className="flex gap-4">
+      <ul className="hidden md:flex md:gap-4">
         {LINKS.map((link) => (
           <>
             {!link.isPage ? (
@@ -50,6 +71,37 @@ const Nav = () => {
           </>
         ))}
       </ul>
+      <img
+        src={HamburgerMenu}
+        alt="hamburger menu"
+        className="flex md:hidden cursor-pointer"
+        onClick={showLinksHandler}
+      />
+      {showLinks && (
+        <div className="fixed top-0 left-0 w-screen h-screen bg-primary_1/90 text-primary_2 z-40">
+          <img
+            src={CloseIcon}
+            alt="close icon"
+            className="w-[64px] absolute top-10 left-10"
+            onClick={() => setShowLinks(false)}
+          />
+          <ul className="text-center top-[50%] -translate-y-1/2 relative flex flex-col gap-10 font-serif text-3xl">
+            {LINKS.map((link) => (
+              <>
+                {!link.isPage ? (
+                  <li onClick={() => setShowLinks(false)}>
+                    <a href={`#${link.link}`}>{link.label}</a>
+                  </li>
+                ) : (
+                  <li onClick={() => setShowLinks(false)}>
+                    <Link to={link.link}>{link.label}</Link>
+                  </li>
+                )}
+              </>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
